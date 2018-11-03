2097008ccbc15305e1e95103b44f5c6799bd5e27
ChocoTaco <boldfilter@zoho.com>
2018-11-03 13:13:23 -0400

    Corrected file structure

--- Classic/scripts/autoexec/packetsettings.cs
+++ Classic/scripts/autoexec/packetsettings.cs
@@ -1,6 +1,6 @@
 $pref::Net::PacketRateToClient = "32"; //determines how many packets per second sent to each client
 $pref::Net::PacketRateToServer = "32"; //may determine how many packets are allowed from each client
-$pref::Net::PacketSize = "350"; //size of each packet sent to each client, maximum.has no effect on size of packets client send to the server
+$pref::Net::PacketSize = "450"; //size of each packet sent to each client, maximum.has no effect on size of packets client send to the server
 
 
 setlogmode(0);

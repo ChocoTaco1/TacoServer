953b7386af9396ce205e6acdfa2ee174d31dae45
ChocoTaco <boldfilter@zoho.com>
2018-11-13 21:39:50 -0500

    Fixed a double override

--- Classic/scripts/autoexec/antiTurret.cs
+++ Classic/scripts/autoexec/antiTurret.cs
@@ -1,16 +1,32 @@
 //Amount of players on a team to enable turrets
 //$Host::EnableTurretPlayerCount = 10;
+//
+//Disable MortarTurret
+//$Host::EnableMortarTurret = 0;
+//
+//Disable = 0
+//Enable  = 1
 
-package antiTurret {
+package antiTurret 
+{
 
 function TurretData::selectTarget(%this, %turret)
 {
-   if( !$Host::TournamentMode && $TotalTeamPlayerCount < $Host::EnableTurretPlayerCount ) {
-      %turret.clearTarget();
-   }
-   else {
-      Parent::selectTarget(%this, %turret);
-   }
+	
+	if( !$Host::TournamentMode && $TotalTeamPlayerCount < $Host::EnableTurretPlayerCount )
+	{
+		%turret.clearTarget();
+	}
+	else if( $Host::EnableMortarTurret )
+	{
+		Parent::selectTarget(%this, %turret);
+	}
+	//No possibility of mortar turret working if map already has it and its banned.
+	else if( %turret.initialBarrel !$= "MortarBarrelLarge" )
+	{
+		Parent::selectTarget(%this, %turret);
+    }
+	
 }
 
 };
--- Classic/scripts/autoexec/noMortarTurret.cs
+++ Classic/scripts/autoexec/noMortarTurret.cs
@@ -3,9 +3,11 @@
 //
 //Disable = 0
 //Enable  = 1
-//
+
+
 // ban mortar turret from inventory in main gametypes
-if( !$Host::EnableMortarTurret ) {
+if( !$Host::EnableMortarTurret )
+{
 	
 	$InvBanList[CTF, "MortarBarrelPack"] = 1;
 	$InvBanList[CnH, "MortarBarrelPack"] = 1;
@@ -13,18 +15,5 @@ if( !$Host::EnableMortarTurret ) {
 	
 }
 
-package noMortarTurret {
-
-// if a mortar turret somehow makes it into the game, keep it from working
-function TurretData::selectTarget(%this, %turret) {
-	
-    if( %turret.initialBarrel !$= "MortarBarrelLarge" ) {
-        Parent::selectTarget(%this, %turret);
-    }
-	
-}
-
-};
-// Prevent package from being activated if it is already
-if (!isActivePackage(noMortarTurret))
-    activatePackage(noMortarTurret);
+//Initial mortar turret barrel code moved to antiTurret.cs
+//to avoid double override.

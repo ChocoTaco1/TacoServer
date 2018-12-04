2265d1b7a6e5d295a6350ad8539523bcf083e195
ChocoTaco <boldfilter@zoho.com>
2018-12-04 18:41:06 -0500

    Formatting

--- Classic/scripts/autoexec/VoteSound.cs
+++ Classic/scripts/autoexec/VoteSound.cs
@@ -1,11 +1,12 @@
 //Make a sound every so seconds to make sure everyone votes
 
-function VoteSound( %game ) {
-	
-	if($VoteSoundInProgress) {
-	messageAll('', '\c1Vote in Progress: \c0Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %display);
-	//$VoteSoundSchedule = schedule(12000, "VoteSound", %game);
-	schedule(12000, 0, "VoteSound", %game);
+function VoteSound( %game )
+{
+
+	if($VoteSoundInProgress) 
+	{
+		messageAll('', '\c1Vote in Progress: \c0Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %display);
+		schedule(12000, 0, "VoteSound", %game);
 	}
 	else
 	return;

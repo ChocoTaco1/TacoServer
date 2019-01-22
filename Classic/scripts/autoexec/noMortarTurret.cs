76ced50e40817a813bacc5c8e7c6ba9cded94412
ChocoTaco <boldfilter@zoho.com>
2019-01-22 01:47:14 -0500

    Simplified

--- Classic/scripts/autoexec/noMortarTurret.cs
+++ Classic/scripts/autoexec/noMortarTurret.cs
@@ -1,19 +1,10 @@
-//Disable MortarTurret
-//$Host::EnableMortarTurret = 0;
+// Disable MortarTurret
+// $Host::EnableMortarTurret = 0;
 //
-//Disable = 0
-//Enable  = 1
+// Disable = 0
+// Enable  = 1
 
+$InvBanList[CTF, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
+$InvBanList[CnH, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
+$InvBanList[Siege, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
 
-// ban mortar turret from inventory in main gametypes
-if( !$Host::EnableMortarTurret )
-{
-	
-	$InvBanList[CTF, "MortarBarrelPack"] = 1;
-	$InvBanList[CnH, "MortarBarrelPack"] = 1;
-	$InvBanList[Siege, "MortarBarrelPack"] = 1;
-	
-}
-
-//Initial mortar turret barrel code moved to antiTurret.cs
-//to avoid double override.

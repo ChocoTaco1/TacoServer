e82d3015d51dc02382bbe7757c14ebb11b5d77e7
ChocoTaco <boldfilter@zoho.com>
2019-01-22 01:47:29 -0500

    Took out mortarbarrel code

--- Classic/scripts/autoexec/antiTurret.cs
+++ Classic/scripts/autoexec/antiTurret.cs
@@ -1,32 +1,20 @@
-//Amount of players on a team to enable turrets
-//$Host::EnableTurretPlayerCount = 10;
+// Amount of players on a team to enable turrets
+// $Host::EnableTurretPlayerCount = 10;
 //
-//Disable MortarTurret
-//$Host::EnableMortarTurret = 0;
-//
-//Disable = 0
-//Enable  = 1
 
 package antiTurret 
 {
 
 function TurretData::selectTarget(%this, %turret)
 {
-	
 	if( !$Host::TournamentMode && $TotalTeamPlayerCount < $Host::EnableTurretPlayerCount )
 	{
 		%turret.clearTarget();
 	}
-	else if( $Host::EnableMortarTurret )
+	else
 	{
-		Parent::selectTarget(%this, %turret);
-	}
-	//No possibility of mortar turret working if map already has it and its banned.
-	else if( %turret.initialBarrel !$= "MortarBarrelLarge" )
-	{
-		Parent::selectTarget(%this, %turret);
+		parent::selectTarget(%this, %turret);
     }
-	
 }
 
 };

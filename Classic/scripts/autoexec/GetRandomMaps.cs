1f5a2a61603add9462f256249ef637de27a34118
ChocoTaco <boldfilter@zoho.com>
2019-03-15 13:28:21 -0400

    Formatting

--- Classic/scripts/autoexec/GetRandomMaps.cs
+++ Classic/scripts/autoexec/GetRandomMaps.cs
@@ -10,40 +10,40 @@ $GetRandomMapsLoaded = true;
 //CTF
 //
 //1-5
-$RandomMapPick1 = "SmallCrossing"; 		$RandomMapPick2 = "OasisIntensity"; 		$RandomMapPick3 = "Blink"; 					$RandomMapPick4 = "SmallTimeCTF"; 					$RandomMapPick5 = "ArenaDome"; 
+$RandomMapPick1 = "SmallCrossing"; 				$RandomMapPick2 = "OasisIntensity"; 		$RandomMapPick3 = "Blink"; 						$RandomMapPick4 = "SmallTimeCTF"; 						$RandomMapPick5 = "ArenaDome"; 
 //6-10
-$RandomMapPick6 = "HighOctane"; 		$RandomMapPick7 = "S5_Damnation"; 			$RandomMapPick8 = "TWL_Feign"; 				$RandomMapPick9 = "TWL2_Skylight"; 					$RandomMapPick10 = "Prismatic"; 
+$RandomMapPick6 = "HighOctane"; 				$RandomMapPick7 = "S5_Damnation"; 			$RandomMapPick8 = "TWL_Feign"; 					$RandomMapPick9 = "TWL2_Skylight"; 						$RandomMapPick10 = "Prismatic"; 
 //11-15
-$RandomMapPick11 = "Dire"; 				$RandomMapPick12 = "TWL2_JaggedClaw"; 		$RandomMapPick13 = "TWL2_Hildebrand"; 		$RandomMapPick14 = "TWL_WilderZone"; 				$RandomMapPick15 = "TWL_Stonehenge"; 
+$RandomMapPick11 = "Dire"; 						$RandomMapPick12 = "TWL2_JaggedClaw"; 		$RandomMapPick13 = "TWL2_Hildebrand"; 			$RandomMapPick14 = "TWL_WilderZone"; 					$RandomMapPick15 = "TWL_Stonehenge"; 
 //16-20
-$RandomMapPick16 = "TWL2_Ocular"; 		$RandomMapPick17 = "S8_Opus"; 				$RandomMapPick18 = "Mirage"; 				$RandomMapPick19 = "DangerousCrossing_nef"; 		$RandomMapPick20 = "S5_Massive"; 
+$RandomMapPick16 = "TWL2_Ocular"; 				$RandomMapPick17 = "S8_Opus"; 				$RandomMapPick18 = "Mirage"; 					$RandomMapPick19 = "DangerousCrossing_nef"; 			$RandomMapPick20 = "S5_Massive"; 
 //21-25
-$RandomMapPick21 = "TheFray"; 			$RandomMapPick22 = "RoundTheMountain"; 		$RandomMapPick23 = "S5_Mordacity"; 			$RandomMapPick24 = "TWL2_CanyonCrusadeDeluxe"; 		$RandomMapPick25 = "Signal"; 
+$RandomMapPick21 = "TheFray"; 					$RandomMapPick22 = "RoundTheMountain"; 		$RandomMapPick23 = "S5_Mordacity"; 				$RandomMapPick24 = "TWL2_CanyonCrusadeDeluxe"; 			$RandomMapPick25 = "Signal"; 
 //26-30
-$RandomMapPick26 = "S8_Cardiac"; 		$RandomMapPick27 = "CirclesEdge"; 			$RandomMapPick28 = "S5_Icedance"; 			$RandomMapPick29 = "Bulwark"; 						$RandomMapPick30 = "Discord"; 
+$RandomMapPick26 = "S8_Cardiac"; 				$RandomMapPick27 = "CirclesEdge"; 			$RandomMapPick28 = "S5_Icedance"; 				$RandomMapPick29 = "Bulwark"; 							$RandomMapPick30 = "Discord"; 
 //31-35
-$RandomMapPick31 = "MoonDance"; 		$RandomMapPick32 = "Rollercoaster_nef"; 	$RandomMapPick33 = "Logans_Run"; 			$RandomMapPick34 = "TWL_BeachBlitz"; 				$RandomMapPick35 = "TWL2_FrozenGlory"; 
-//36-40
-$RandomMapPick36 = "CircleofStones"; 	$RandomMapPick37 = "TitanV"; 				$RandomMapPick38 = "TWL_Katabatic"; 		$RandomMapPick39 = "TWL2_Magnum"; 					$RandomMapPick40 = "Fenix"; 
+$RandomMapPick31 = "MoonDance"; 				$RandomMapPick32 = "Rollercoaster_nef"; 	$RandomMapPick33 = "Logans_Run"; 				$RandomMapPick34 = "TWL_BeachBlitz"; 					$RandomMapPick35 = "TWL2_FrozenGlory"; 
+//36-40	
+$RandomMapPick36 = "CircleofStones"; 			$RandomMapPick37 = "TitanV"; 				$RandomMapPick38 = "TWL_Katabatic"; 			$RandomMapPick39 = "TWL2_Magnum"; 						$RandomMapPick40 = "Fenix"; 
 //
 //LakRabbit
 //
 //1-3
-$LakRandomMapPick1 = "VaubanLak"; 			$LakRandomMapPick2 = "MiniSunDried"; 		$LakRandomMapPick3 = "TitaniaLak";
+$LakRandomMapPick1 = "VaubanLak"; 				$LakRandomMapPick2 = "MiniSunDried"; 		$LakRandomMapPick3 = "TitaniaLak";
 //4-6
-$LakRandomMapPick4 = "DesertofDeathLak";	$LakRandomMapPick5 = "Sundance";			$LakRandomMapPick6 = "SunDriedLak";
+$LakRandomMapPick4 = "DesertofDeathLak";		$LakRandomMapPick5 = "Sundance";			$LakRandomMapPick6 = "SunDriedLak";
 //7-9
-$LakRandomMapPick7 = "SkinnyDipLak"; 		$LakRandomMapPick8 = "LushLak";				$LakRandomMapPick9 = "InfernusLak";
+$LakRandomMapPick7 = "SkinnyDipLak"; 			$LakRandomMapPick8 = "LushLak";				$LakRandomMapPick9 = "InfernusLak";
 //10-12
-$LakRandomMapPick10 = "Arrakis"; 			$LakRandomMapPick11 = "BoxLak"; 			$LakRandomMapPick12 = "TreasureIslandLak";
+$LakRandomMapPick10 = "Arrakis"; 				$LakRandomMapPick11 = "BoxLak"; 			$LakRandomMapPick12 = "TreasureIslandLak";
 //13-15
-$LakRandomMapPick13 = "Raindance_nefLak"; 	$LakRandomMapPick14 = "SaddiesHill"; 		$LakRandomMapPick15 = "TWL_BeachBlitzLak"; 
+$LakRandomMapPick13 = "Raindance_nefLak"; 		$LakRandomMapPick14 = "SaddiesHill"; 		$LakRandomMapPick15 = "TWL_BeachBlitzLak"; 
 //16-18
-$LakRandomMapPick16 = "PhasmaDustLak"; 		$LakRandomMapPick17 = "Sulfide"; 			$LakRandomMapPick18 = "HavenLak"; 
+$LakRandomMapPick16 = "PhasmaDustLak"; 			$LakRandomMapPick17 = "Sulfide"; 			$LakRandomMapPick18 = "HavenLak"; 
 //19-21
-$LakRandomMapPick19 = "Crater71Lak"; 		$LakRandomMapPick20 = "SolsDescentLak"; 	$LakRandomMapPick21 = "FrozenFuryLak"; 
+$LakRandomMapPick19 = "Crater71Lak"; 			$LakRandomMapPick20 = "SolsDescentLak"; 	$LakRandomMapPick21 = "FrozenFuryLak"; 
 //22-24
-$LakRandomMapPick22 = "CankerLak"; 			$LakRandomMapPick23 = "S8_GeothermalLak"; 	$LakRandomMapPick24 = "EquinoxLak"; 
+$LakRandomMapPick22 = "CankerLak"; 				$LakRandomMapPick23 = "S8_GeothermalLak"; 	$LakRandomMapPick24 = "EquinoxLak"; 
 //
 //LCTF
 //

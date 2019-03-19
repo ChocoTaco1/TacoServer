3555722a81e671b25b4fae48bd45442f86a56e7f
ChocoTaco <boldfilter@zoho.com>
2019-03-19 02:41:04 -0400

    Votekick notify team specific

--- Classic/scripts/autoexec/VoteSound.cs
+++ Classic/scripts/autoexec/VoteSound.cs
@@ -22,9 +22,28 @@ function VoteSound( %game, %typename, %arg1, %arg2, %VoteSoundRandom )
 				if(%arg1 $= "999") %arg1 = "unlimited";
 				messageAll('', '\c1Vote in Progress: \c0To change the time limit to %1. Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %arg1 );
 				
-			case "VoteKickPlayer":
-				messageAll('', '\c1Vote in Progress: \c0To kick player %1. Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %arg1.name );
-				
+			case "VoteKickPlayer":			
+				if(%arg1.team != 0 && Game.numTeams > 1) //Not observer
+				{
+				   for(%idx = 0; %idx < ClientGroup.getCount(); %idx++) 
+				   {
+					  %cl = ClientGroup.getObject(%idx);
+					  
+						if (%cl.isAdmin == true)
+						{ 
+							if(%cl.team !$= %arg1.team) //Not on admins team
+							{
+								messageClient(%cl, '', '\c1Vote in Progress: \c0To kick %1 on the other team.~wgui/objective_notification.wav', %arg1.name);
+							}
+							else //Is on admins team
+								messageClient(%cl, '', '\c1Vote in Progress: \c0To kick player %1. Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %arg1.name );
+						}
+						else if(%cl.team $= %arg1.team) //Everyone else
+							messageClient(%cl, '', '\c1Vote in Progress: \c0To kick player %1. Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %arg1.name );
+					}
+				}
+				else //Is observer
+					messageAll('', '\c1Vote in Progress: \c0To kick player %1. Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %arg1.name );
 			case "VoteTournamentMode":
 				messageAll('', '\c1Vote in Progress: \c0To change the mission to Tournament Mode (%1). Press Insert for Yes or Delete for No.~wgui/objective_notification.wav', %arg1, %arg2 );
 		}

3c8959482d4fc8e7c8a5d5840b6e714ddf977dec
ChocoTaco <boldfilter@zoho.com>
2019-08-21 15:51:03 -0400

    Allow Bot skins

--- Classic/scripts/server.cs
+++ Classic/scripts/server.cs
@@ -789,12 +789,13 @@ function GameConnection::onConnect( %client, %name, %raceGender, %skin, %voice, 
    else
       %client.nameBase = %realName;
 
+   //Allow - ChocoTaco
    // Make sure that the connecting client is not trying to use a bot skin:
-   %temp = detag( %skin );
-   if ( %temp $= "basebot" || %temp $= "basebbot" )
-      %client.skin = addTaggedString( "base" );
-   else
-      %client.skin = addTaggedString( %skin );
+   //%temp = detag( %skin );
+   //if ( %temp $= "basebot" || %temp $= "basebbot" )
+   //   %client.skin = addTaggedString( "base" );
+   //else
+        %client.skin = addTaggedString( %skin );
 
    %client.voice = %voice;
    %client.voiceTag = addtaggedString(%voice);

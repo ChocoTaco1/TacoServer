22ec908b394930182770302d45d3271206c9d569
ChocoTaco <boldfilter@zoho.com>
2019-11-11 12:46:07 -0500

    MortarTurret Console Toggle

--- Classic/scripts/autoexec/AntiTurret.cs
+++ Classic/scripts/autoexec/AntiTurret.cs
@@ -42,6 +42,20 @@ function TurretData::selectTarget(%this, %turret)
 if (!isActivePackage(AntiTurret))
     activatePackage(AntiTurret);
 
+
 $InvBanList[CTF, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
 $InvBanList[CnH, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
 $InvBanList[Siege, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
+
+// To be run in-game thru console to update Mortar Turret status
+function ToggleMortarTurret()
+{
+	if($Host::EnableMortarTurret $= 0)
+		$Host::EnableMortarTurret = 1;
+	else
+		$Host::EnableMortarTurret = 0;
+	
+	$InvBanList[CTF, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
+	$InvBanList[CnH, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
+	$InvBanList[Siege, "MortarBarrelPack"] = !$Host::EnableMortarTurret;
+}

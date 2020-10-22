2082b95858c0f58c1c66276c7d3b33b733e96db3
ChocoTaco1 <boldfilter@zoho.com>
2020-10-22 15:58:38 -0400

    Added cmdArmor patch

--- Classic/scripts/autoexec/MemPatches.cs
+++ Classic/scripts/autoexec/MemPatches.cs
@@ -25,3 +25,35 @@ function suppressTraversalRootPatch()
    memPatch("56D114", "90909090909090909090909090909090909090909090");
    $tvpatched = 1;
 }
+
+if (!$CmdArmor::Patched)
+{
+	$CmdArmor::Patched = true;
+	//memPatch("6FC746", "66B8000090906683FE017408ACAA84C075FA89D05F5EC3");
+	memPatch("6FC746", "83FE017408ACAA84C075FA89D05F5EC3");
+	//Removed register size override (cmp si, 1 ->  cmp esi, 1) and got rid of
+	//weird NASM garbage code at the beginning. Had a mov ax, 0 which did nothing
+	//and wasn't necessary anyways because of xor eax, eax in the original. It also
+	//generated several NOPs after that for no reason.
+}
+
+function serverCmd(%client)
+{
+	// Stick your own administrative action code here
+	messageAll('msgAll',"\c3" @ %client.namebase SPC "is attempting to crash the server!");
+
+	messageClient(%client, 'onClientBanned', "");
+	messageAllExcept( %client, -1, 'MsgClientDrop', "", %client.name, %client );
+   
+	// kill and delete this client
+	if( isObject(%client.player) )
+		%client.player.scriptKill(0);
+   
+	if ( isObject( %client ) )
+	{
+		%client.setDisconnectReason("You have been banned for attempting to crash the server.");
+		%client.schedule(700, "delete");
+	}
+   
+	BanList::add(%client.guid, %client.getAddress(), $Host::BanTime);
+}

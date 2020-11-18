201c0ba03c464dedbfd24078c5f72cc5f4b4faea
ChocoTaco1 <boldfilter@zoho.com>
2020-11-18 13:04:08 -0500

    Oops

--- Classic/scripts/autoexec/altWarmup.cs
+++ Classic/scripts/autoexec/altWarmup.cs
@@ -8,7 +8,7 @@
 // Enable or Disable
 $AW::EnableALTWarmUp = 1;
 // Normal Warmup Time (In Seconds) - 20 is 20 Seconds
-$AW::DefaultWarmUpTime = 20
+$AW::DefaultWarmUpTime = 20;
 // The amount of time you want to allow for players to switch teams (In seconds) - 60 is 60 seconds
 $AW::ALTWarmUpTime = 60;
 // Minimum Population to Activate

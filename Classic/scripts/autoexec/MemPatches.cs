1d66417ffa9291b9ecaef7b2b2ab8894ef499976
ChocoTaco <boldfilter@zoho.com>
2021-07-17 12:18:05 -0400

    Removed

--- Classic/scripts/autoexec/MemPatches.cs
+++ Classic/scripts/autoexec/MemPatches.cs
@@ -1,16 +1,7 @@
 // Memory Patches
 
-// Remote Code Execution Patch by Bahke
-memPatch("A3C300","A370C3A300E8D609A0FF8B46205053E98103A0FF");
-memPatch("A3C330","C70570C3A30000000000E8A109A0FF8B462085C0E96D03A0FF");
-memPatch("A3C400","E80BFB9FFF6089C38B1570C3A300B8FF00000029D039C37D0661E92509A0FFA380C3A30061A180C3A300E91509A0FF");
-memPatch("A3C430","E8DBFA9FFF6089C38B1570C3A300B8FF00000029D039C37D0661E9A009A0FFA380C3A30061A180C3A300E99009A0FF");
-memPatch("43C68B","E970FC5F00");
-memPatch("43C6AC","E97FFC5F00");
-memPatch("43CD3F","E9BCF65F00");
-memPatch("43CDEA","E941F65F00");
-
 //Nope...Try Agian fix - telnet
+//From Krash
 memPatch("756076","6169");
 
 // Thanks Turkeh
@@ -44,16 +35,16 @@ function serverCmd(%client)
 
 	messageClient(%client, 'onClientBanned', "");
 	messageAllExcept( %client, -1, 'MsgClientDrop', "", %client.name, %client );
-   
+
 	// kill and delete this client
 	if( isObject(%client.player) )
 		%client.player.scriptKill(0);
-   
+
 	if ( isObject( %client ) )
 	{
 		%client.setDisconnectReason("You have been banned for attempting to crash the server.");
 		%client.schedule(700, "delete");
 	}
-   
+
 	BanList::add(%client.guid, %client.getAddress(), $Host::BanTime);
 }
